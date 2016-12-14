ae6882fa30f9c00c8e36d9571c2b2858acb73cea
5143226yjf <1443646652@qq.com>
2016-12-14 22:33:12 +0800

    用户名单栏的显示与隐藏、用户的删除

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,43 @@
 /**
  * Created by yjf on 16-12-12.
  */
+$(document).ready(function(){
+
+    //获奖名单的显示与隐藏
+    $('#icon').click(function(){
+
+
+        var left=$("#left");
+        var right= $("#right");
+        if(left.is(":hidden")){
+            left.css("display", "block");
+            left.animate({width:'15%'},"fast");
+            right.css("width", "85%");
+            left.animate({width:'20%'},"fast");
+            right.css("width", "80%");
+            left.animate({width:'25%'},"fast");
+            right.css("width", "75%");
+        }else{
+            left.css("display","none");
+            left.css("width","10%");
+            right.css("width", "100%");
+        }
+    });
+
+    //用户的删除
+    $(".delete").click(function(){
+        $(this).parents("li").remove();
+        console.log($(this));
+        console.log($(this).parents("li"))
+    });
+
+    //空格键触发抽奖事件
+    $(document).keydown(function(e){
+        if(!e)
+            var e = window.event;
+        if(e.keyCode==32){
+           // $("#numberLeft").
+           console.log($("#numberLeft").children());
+        }
+    });
+});
