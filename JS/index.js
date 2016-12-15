e2ae2bca370e26b726809af983332d4da0045b26
5143226yjf <1443646652@qq.com>
2016-12-15 17:10:47 +0800

    用户名单的初始化，抽奖功能的实现

--- JS/index.js
+++ JS/index.js
@@ -3,10 +3,36 @@
  */
 $(document).ready(function(){
 
+    var usePeople=[
+        {tel:"187***1005",name:"忘川",img:"images/data-pic1.jpg/"},
+        {tel:"183***1005",name:"聆空",img:"images/data-pic2.jpg/"},
+        {tel:"187***8113",name:"初夏薄荷",img:"images/data-pic3.jpg"},
+        {tel:"187***4792",name:"风过无痕",img:"images/data-pic4.jpg"},
+        {tel:"135***7674",name:"后会无期",img:"images/data-pic5.jpg"},
+        {tel:"159***2235",name:"梨花落",img:"images/data-pic6.jpg"},
+        {tel:"156***3650",name:"如果巴黎不快乐",img:"images/data-pic7.jpg"},
+        {tel:"131***9126",name:"someone",img:"images/data-pic8.jpg"},
+        {tel:"187***5338",name:"小清新",img:"images/data-pic9.jpg"}
+    ];
+
+    //初始化
+    (function () {
+        var ul=$('#users');
+        var li =$ ('.user');
+        console.log(usePeople.length);
+        for(var a=0;a < usePeople.length; a++){
+            li.find('#img').attr("src",usePeople[a].img);
+            li.find('.data-num').text(usePeople[a].tel);
+            li.find('.date-name').text(usePeople[a].name);
+            ul.append(li.clone(true));
+            $(".counter").text(usePeople.length);
+        }
+    })();
+
+
     //获奖名单的显示与隐藏
     $('#icon').click(function(){
-
-
+        $("#first").parents("li").remove();
         var left=$("#left");
         var right= $("#right");
         if(left.is(":hidden")){
@@ -27,17 +53,63 @@ $(document).ready(function(){
     //用户的删除
     $(".delete").click(function(){
         $(this).parents("li").remove();
-        console.log($(this));
-        console.log($(this).parents("li"))
+        var counter=$('.counter').text()-1;
+        $(".counter").text(counter);
     });
+
+    //自定义随机数
+    function randomNum(minNum,maxNum){
+        switch(arguments.length){
+            case 1:
+                return parseInt(Math.random()*minNum+1);
+                break;
+            case 2:
+                return parseInt(Math.random()*(maxNum-minNum+1)+minNum);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+
+    var draw = 0;
+    var count=0;
+    var index=0;
 
     //空格键触发抽奖事件
     $(document).keydown(function(e){
         if(!e)
             var e = window.event;
         if(e.keyCode==32){
-           // $("#numberLeft").
-           console.log($("#numberLeft").children());
+            if(count%2==0){
+                var numbers1=$("#numberLeft li");
+                var numbers2=$("#numberRight li");
+                draw= setInterval(function () {
+                    var num=randomNum(0,8);
+                    $('.head-pic').children('img').attr("src",usePeople[num].img);
+                    var string = usePeople[num].tel.split("");
+                    console.log(string);
+                    for(i in numbers1){
+                        numbers1[i].innerHTML=string[i];
+                    }
+                    for(j in numbers2){
+                        numbers2[j].innerHTML=string[parseInt(j)+6];
+                    }
+                    index=num;
+                },50);
+                count++;
+            }
+            else if(count%2==1){
+                clearInterval(draw);
+                var lucker=$("#luckUser");
+                lucker.css("visibility"," visible");
+                lucker.find('#pic').attr("src",usePeople[index].img);
+                lucker.find('.data-num').text(usePeople[index].tel);
+                lucker.find('.date-name').text(usePeople[index].name);
+                count++;
+            }
+
+
         }
     });
 });
